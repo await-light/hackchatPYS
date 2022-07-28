60065e23f5ebd045072e46f1634370821d690e8b
Light <mailllj@163.com>
2022-07-28 16:02:16 +0800

    but failed to load

---
+++ core/color.py
@@ -0,0 +1,20 @@
+
+
+import sys
+sys.path.append("../")
+import base
+
+
+class Color(base.CommandBase):
+	def __init__(self,websocket,users,data):
+		self.argslist = ["color"]
+		base.CommandBase.__init__(self,
+			websocket=websocket,
+			users=users,
+			data=data)
+
+	def execute(self):
+		if "color" in self.data:
+			for user in self.users.userset:
+				if user.websocket == self.websocket:
+					user.color = self.data["color"]
