895c652fe1483bdcf5a592f6c00420fd93cf69a7
Light <mailllj@163.com>
2022-07-28 13:14:07 +0800

    improve join data

--- core/join.py
+++ core/join.py
@@ -18,34 +18,34 @@ class Join(base.CommandBase):
 			data=data)
 
 	def if_correct_format_data(self,data):
+		'''
+		correct data result is True
+		otherwise result is False
+		'''
 		if ("nick" and "channel") in data:
 			return True
 		return False
 
 	def judge_nick(self,nick):
-		'''
-		if correct return None
-		else return error message
-		'''
 		# judge whether it has password
-		passwordfalsepattern = r"^([a-zA-Z0-9_]{1,24})$"
-		passwordtruepattern = r"^([a-zA-Z0-9_]{1,24}) #(.+)$"
+		passwordfalsepattern = r"^([a-zA-Z0-9_]{1,24})$" # no password
+		passwordtruepattern = r"^([a-zA-Z0-9_]{1,24}) #(.+)$" # with password
 		falseresult = re.findall(passwordfalsepattern,nick)
 		trueresult = re.findall(passwordtruepattern,nick)
-		correct = True
+		correct = True # correct(don't have or have)
 
 		if falseresult != []:
-			self.nick,password = falseresult[0],None
+			newnick,password = falseresult[0],None # if no password,password = None
 		elif trueresult != []:
-			self.nick,password = trueresult[0]
+			newnick,password = trueresult[0]
 		else: # if it doesn't have correct nick 
 			correct = False 
 
 		if correct: 
 			if password == None: 
 				# the password is None,so the trip is None
-				self.trip = None
-				return None 
+				trip = None # <- trip
+				return (newnick,trip) 
 
 			else: # first sha256 and then base64
 				'''
@@ -58,8 +58,9 @@ class Join(base.CommandBase):
 				};
 				'''
 				s2pwd = hashlib.sha256(password.encode()).hexdigest()
-				self.trip = base64.b64encode(s2pwd.encode()).decode()[:6]
-				return None
+				trip = base64.b64encode(s2pwd.encode()).decode()[:6]
+				return (newnick,trip)
+
 		else: # 
 			logging.info("%s tried to join,but failed" % self.websocket)
 			return json.dumps({
@@ -71,16 +72,26 @@ class Join(base.CommandBase):
 	def execute(self):
 		# if the json data is correct,set self.nick and self.channel
 		if self.if_correct_format_data(self.data):
-			self.nick = self.data["nick"]
-			self.channel = self.data["channel"]
+			handlenick = self.data["nick"]
+			channel = self.data["channel"]
 		# Nothing will happen if the json data is incorrect
 		else:
 			return None
 		
 		# 
-		nickcorrect = self.judge_nick(self.nick)
-		if nickcorrect != None: # error message
-			return nickcorrect
+		nickcorrect = self.judge_nick(handlenick)
+
+		'''
+		if nickcorrect's type is json
+		> send json data
+
+		if nickcorrect's type is tuple
+		> newnick , trip 
+		'''
+		if str(type(nickcorrect)) == "<class 'str'>":
+			return nickcorrect # <- json data
+		elif str(type(nickcorrect)) == "<class 'tuple'>": 
+			newnick,trip = nickcorrect
 
 		# self.websocket
 		# self.users
@@ -90,51 +101,64 @@ class Join(base.CommandBase):
 
 		# 1. the nick is not in the channel
 		# 2. the websocket doesn't enter any channel before
-		channelnicks = [user.nick.lower() for user in self.users.userset if user.channel == self.channel]
+		channelnicks = [user.nick.lower() for user in self.users.userset if user.channel == channel]
 		websockethis = [user.websocket for user in self.users.userset]
 
 		if self.websocket not in websockethis:
-			if self.nick.lower() not in channelnicks:
+			if newnick.lower() not in channelnicks:
 				init = base.User(
 						websocket=self.websocket,
-						nick=self.nick,
-						trip=self.trip,
-						channel=self.channel)
+						nick=newnick,
+						trip=trip,
+						channel=channel)
 
-				self.users.broadcasttext(self.channel,
-						json.dumps({
-							"cmd":"onlineAdd",
-							"nick":init.nick,
-							"trip":init.trip,
-							"uType":init.utype,
-							"hash":init.hash_,
-							"level":init.level,
-							"userid":init.userid,
-							"isbot":init.isbot,
-							"color":init.color,
-							"channel":init.channel,
-							"time":round(time.time())
-							}))
+				broadcastdata = {
+					"cmd":"onlineAdd",
+					"nick":init.nick,
+					"uType":init.utype,
+					"hash":init.hash_,
+					"level":init.level,
+					"userid":init.userid,
+					"isbot":init.isbot,
+					"channel":init.channel,
+					"time":round(time.time())
+					}
+
+				if init.trip != None:
+					broadcastdata["trip"] = init.trip
+
+				self.users.broadcasttext(channel,
+					json.dumps(broadcastdata))
 					
 				self.users.userset.add(init)
 
-				logging.info("%s joined" % self.nick)
-					
-				return json.dumps({
+				logging.info("%s joined %s" % (newnick,channel))
+
+				resultdata = {
 					"cmd":"onlineSet",
-					"nicks":[user.nick for user in self.users.userset if user.channel == self.channel],
-					"users":[{
+					"nicks":[user.nick for user in self.users.userset if user.channel == channel],
+					"users":[]}
+
+				for user in self.users.userset:
+					data = {
 						"channel":user.channel,
 						"isme":user.isme,
 						"nick":user.nick,
-						"trip":user.trip,
 						"uType":user.utype,
 						"hash":user.hash_,
 						"level":user.level,
 						"userid":user.userid,
 						"isbot":user.isbot,
 						"color":user.color
-						} for user in self.users.userset]})
+						}
+					if user.trip != None:
+						data["trip"] = user.trip
+					if user.color != None:
+						data["color"] = user.color
+
+					resultdata["users"].append(data)
+					
+				return json.dumps(resultdata)
 
 			else:
 				logging.info("%s tried to join,but failed" % self.websocket)
