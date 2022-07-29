5a70087f8ef6e95f179fa6768b3d2d5c3beeabd9
Light <mailllj@163.com>
2022-07-29 11:42:48 +0800

    set __all__

--- internal/__init__.py
+++ internal/__init__.py
@@ -1,8 +1,1 @@
-import os
-import importlib
-
-path = "."
-files = os.listdir(path)
-for file in files:
-	if file[-3:] == ".py" and file != "__init__.py":
-		exec("%s = importlib.import_module(file[:-3])" % file[:-3])
+__all__ = ["left"]
