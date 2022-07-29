d54619bc956a61c75f864e389cc09f52067ed7d2
Light <mailllj@163.com>
2022-07-29 15:24:10 +0800

    don't need to change the code when adding a new command except a new folder

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,3 @@
-__all__ = [
-	"chat",
-	"color",
-	"emote",
-	"join",
-	"whisper"
-	]
+import os
+__all__ = [file[:-3] for file in os.listdir(__file__[:-11]) 
+	if file[-3:] == ".py" and file != "__init__.py"]
--- internal/__init__.py
+++ internal/__init__.py
@@ -1,1 +1,3 @@
-__all__ = ["left"]
+import os
+__all__ = [file[:-3] for file in os.listdir(__file__[:-11]) 
+	if file[-3:] == ".py" and file != "__init__.py"]
