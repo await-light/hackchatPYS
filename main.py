b37368759230ea12105b2feada8eb1ac94f4fd19
Lightyoo <mailllj@163.com>
2022-08-23 03:06:34 +0000

    replace emoji shortcode

--- main.py
+++ main.py
@@ -26,11 +26,14 @@ class UserList:
 		# return all User objects that .channel == channel
 		return [user for user in self.userlist if user.channel == channel]
 
+with open("./emoji.txt","r") as f:
+	emojis = dict(re.findall(r"(.) (:[a-zA-Z_0-9]+:)\n",f.read()))
+	
 userlist = UserList()
 salt = b'''j\'[5:],u~$??ioveu6$?$w/jqp/^bp\\coeu8"<i"~&<+2t;i3\\:h8;-2]]z~{a.c,
 \\e-38%$ng7^+:\\5kq#d\\\\}uu|!sm7~;#!bq\'!@\'25y|>n/zxk#b8;@)!#zl.b-:~~@u{8:&g&
 "b?pj,_ibp)o+xv(8._{33d1iy#_`k5pn!jn+/#w~h~s4(=8!lo+r,gvxm&&"47604wn*df7:v52^+b
-"}n|o{cgdxf`3_l71rcd(q=7*w1r:yw&\\y$5:wao?3e/ymvb-%.^d,ta#_33zk,;"o&<|3025"h,ox
+"}n|o{cgdxf`3_l71rcd(q=7*w1re:yw&\\y$5:wao?3e/ymvb-%.^d,ta#_33zk,;"o&<|3025"h,ox
 (#"u&xrljg(6^e,sj@v|@.i5u7*z0~`t(x@a)-4&+m|+|;3,{*4\\j`<i~|=`*<w"l]}iw,2\'d/h=r8
 !-\'<xd_\\+tc%eqg.*]t;[|v?1,e:q@#.d"cc]w*"m:7~p\'>\'1$3s4|7>xc6rg`j++!@^e?!yv<b>
 ~-8_l@`p(6%>%;^y:6,/kb{@we_jnhtw5yi7);~?~5>[h@/_n^3'''
@@ -42,7 +45,7 @@ async def join(websocket,data,userlist):
 	# channel,nick,trip
 	channel = data["channel"]
 	try:
-		nick,password = re.findall(r"^([a-zA-Z_]{1,24}) ?(#.+)?$",
+		nick,password = re.findall(r"^([a-zA-Z_0-9]{1,24}) ?(#.+)?$",
 			data["nick"])[0]
 	except:
 		await websocket.send(json.dumps({
@@ -131,22 +134,23 @@ async def chat(websocket,data,userlist):
 	else:
 		text = data["text"]
 
-	if text[0] == "/":
+	if text.startswith("/"):
 		text = text[1:]
+		if not text.startswith("/"):
+			if text == "shrug":
+				text = r"¯\\\_(ツ)\_/¯"
+			else:
+				await websocket.send(
+					json.dumps({
+						"cmd":"warn",
+						"text":"Unknown command: /%s" % text,
+						"channel":False,
+						"time":time.time()
+						}))
+				return None
 
-		if text == "shrug":
-			text = r"¯\\\_(ツ)\_/¯"
-		elif text == "cry":
-			text = r"T^T"
-		else:
-			await websocket.send(
-				json.dumps({
-					"cmd":"warn",
-					"text":"Unknown command: /%s" % text,
-					"channel":False,
-					"time":time.time()
-					}))
-			return None
+	for emoji,shortcode in emojis.items():
+		text = text.replace(shortcode,emoji)
 
 	websockets.broadcast(
 		[u.websocket for u in userlist.channel(user.channel)],
