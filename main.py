b5f947d9543ad17a5ec08c925f2562709956b0e3
Lightyoo <mailllj@163.com>
2022-08-29 05:30:41 +0000

    time outt

--- main.py
+++ main.py
@@ -506,13 +506,8 @@ if __name__ == '__main__':
 	with open("./emoji.txt","r",encoding="utf-8") as f:
 		emojis = dict(re.findall(r"([^ ]+) (\:[a-zA-Z0-9_-]+\:) ?\n",f.read()))
 	userlist = UserList()
-	salt = b'''j\'[5:],u~$??ioveu6$?$w/jqp/^bp\\coeu8"<i"~&<+2t;i3\\:h8;-2]]z~{a.c,
-	\\e-38%$ng7^+:\\5kq#d\\\\}uu|!sm7~;#!bq\'!@\'25y|>n/zxk#b8;@)!#zl.b-:~~@u{8:&g&
-	"b?pj,_ibp)o+xv(8._{33d1iy#_`k5pn!jn+/#w~h~s4(=8!lo+r,gvxm&&"47604wn*df7:v52^+b
-	"}n|o{cgdxf`3_l71rcd(q=7*w1re:yw&\\y$5:wao?3e/ymvb-%.^d,ta#_33zk,;"o&<|3025"h,ox
-	(#"u&xrljg(6^e,sj@v|@.i5u7*z0~`t(x@a)-4&+m|+|;3,{*4\\j`<i~|=`*<w"l]}iw,2\'d/h=r8
-	!-\'<xd_\\+tc%eqg.*]t;[|v?1,e:q@#.d"cc]w*"m:7~p\'>\'1$3s4|7>xc6rg`j++!@^e?!yv<b>
-	~-8_l@`p(6%>%;^y:6,/kb{@we_jnhtw5yi7);~?~5>[h@/_n^3'''
+	with open("./data/salt.txt","r") as fp:
+		salt = fp.read().encode()
 	
 	islockall = False
 	lockroom_list = []
